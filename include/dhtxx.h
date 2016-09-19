fb88b8c86aaa1965b295078c8dfabe3f07784f21
Jacek Wieczorek <mrjjot@gmail.com>
2016-09-19 09:26:41 +0200

    Add license headers to files

--- include/dhtxx.h
+++ include/dhtxx.h
@@ -1,3 +1,11 @@
+/* dhtxx.h - a part of avr-dhtxx library
+ *
+ * Copyright (C) 2016 Jacek Wieczorek
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
 #ifndef DHTXX_H
 #define DHTXX_H
 
